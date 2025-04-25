d42dd0d45ae02c98f6b3f08ef384eb0822eb689c
Solorsi <sorsifernandez@udesa.edu.ar>
2025-04-24 22:05:15 -0300

    se agregaron todos los productos

--- src/data/products.js
+++ src/data/products.js
@@ -1,32 +1,116 @@
 const products = [
     {
       id: 1,
-      name: "Vestido Floral",
-      price: 12000,
+      name: "Tapado Peludo",
+      price: 50000,
       description: "Vestido largo con flores coloridas.",
-      images: ["/vestido1.jpg"]
+      images: ["/productos/Abrigos/peludito1.jpeg", "/productos/Abrigos/peludito2.jpeg"]
     },
     {
       id: 2,
-      name: "Top Rosa",
-      price: 8000,
+      name: "Pollera Aries",
+      price: 20000,
       description: "Top rosa con tirantes.",
-      images: ["/top1.jpg"]
+      images: ["/productos/PollerasYShorts/polleraAries1.jpeg", "/productos/PollerasYShorts/polleraAries2.jpeg"]
     },
     {
       id: 3,
-      name: "Short Denim",
-      price: 10000,
+      name: "Short Cruzado",
+      price: 30000,
       description: "Short de jean celeste con bordes deshilachados.",
-      images: ["/short1.jpg"]
+      images: ["/productos/PollerasYShorts/shortCruzado.jpeg"]
     },
     {
       id: 4,
       name: "Set Silver",
-      price: 13900,
+      price: 33000,
       description: "Set de dos piezas color negro con apliques plateados.",
-      images: ["/setSilver.jpeg" , "logo2.png"]
-    }
+      images: ["/productos/Sets/setSilver.jpeg"]
+    },
+    {
+      id: 5,
+      name: "Top de Jean",
+      price: 20000,
+      description: "Set de dos piezas color negro con apliques plateados.",
+      images: ["/productos/TopsYRemeras/topJean1.jpeg" , "/productos/TopsYRemeras/topJean1.jpeg"]
+    },
+    {
+      id: 6,
+      name: "Remera Cruzada",
+      price: 24000,
+      description: "Set de dos piezas color negro con apliques plateados.",
+      images: ["/productos/TopsYRemeras/remeCruzada1.jpeg" , "/productos/TopsYRemeras/remeCruzada1.jpeg"]
+    },
+    {
+      id: 7,
+      name: "Top Sara Marron",
+      price: 21000,
+      description: "Set de dos piezas color negro con apliques plateados.",
+      images: ["/productos/TopsYRemeras/topSaraMarron.jpeg"]
+    },
+    {
+      id: 8,
+      name: "Top Silver Blanco",
+      price: 20000,
+      description: "Set de dos piezas color negro con apliques plateados.",
+      images: ["/productos/TopsYRemeras/topSilverBlanco.jpeg"]
+    },
+    {
+      id: 9,
+      name: "Top Sara Negro",
+      price: 21000,
+      description: "Set de dos piezas color negro con apliques plateados.",
+      images: ["/productos/TopsYRemeras/topSaraNegro.jpeg"]
+    },
+    {
+      id: 10,
+      name: "Vestido Capricornio",
+      price: 28000,
+      description: "Set de dos piezas color negro con apliques plateados.",
+      images: ["/productos/Vestidos/vestidoCapricornio.jpeg"]
+    },
+    {
+      id: 11,
+      name: "Vestido Silver",
+      price: 25000,
+      description: "Set de dos piezas color negro con apliques plateados.",
+      images: ["/productos/Vestidos/vestidoSilver1.jpeg", "/productos/Vestidos/vestidoSilver2.jpeg"]
+    },
+    {
+      id: 12,
+      name: "Vestido Vienna",
+      price: 26000,
+      description: "Set de dos piezas color negro con apliques plateados.",
+      images: ["/productos/Vestidos/vestidoVienna1.jpeg" , "/productos/Vestidos/vestidoVienna2.jpeg"]
+    },
+    {
+      id: 13,
+      name: "Set Sienna",
+      price: 33000,
+      description: "Set de dos piezas color negro con apliques plateados.",
+      images: ["/productos/Sets/setSienna1.jpeg", "/productos/Sets/setSienna2.jpeg"]
+    },
+    {
+      id: 14,
+      name: "Set Sophie",
+      price: 33000,
+      description: "Set de dos piezas color negro con apliques plateados.",
+      images: ["/productos/Sets/setSophie1.jpeg", "/productos/Sets/setSophie1.jpeg"]
+    },
+    {
+      id: 15,
+      name: "Top Silver Negro",
+      price: 20000,
+      description: "Set de dos piezas color negro con apliques plateados.",
+      images: ["/productos/TopsYRemeras/topSilverNegro.jpeg"]
+    },
+    {
+      id: 16,
+      name: "Short Silver",
+      price: 20000,
+      description: "Set de dos piezas color negro con apliques plateados.",
+      images: ["/productos/PollerasYShorts/shortSilver.jpeg"]
+    },
   ];
   
   export default products;
